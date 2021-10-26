2b250aa6e56d3a817be71651bcff8e76923bb9da
janvi18 <sjanvi9333@gmail.com>
2021-10-26 17:47:46 +0530

    .

--- controller/forgetpassController.js
+++ controller/forgetpassController.js
@@ -1,6 +1,7 @@
 var forgetPass = require("../model/forgetpassModel")
 var mongoose = require("mongoose")
 
+var User = require("../model/userModel")
 exports.forgotPassword = function (req, res) {
     User.updateOne({ _id: req.body._id }, { $set: { password: req.body.password } }, function (err, data) {
         if (err) {
