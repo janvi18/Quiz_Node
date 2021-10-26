1d6166fc409e36bdf16f37bd44fa84ff8b7846aa
janvi18 <sjanvi9333@gmail.com>
2021-10-26 14:43:21 +0530

    .

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://localhost:27017/nodeexam', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect('mongodb://localhost:27017/nodeexam', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://janvi123:janvi123@cluster0.l3iat.mongodb.net/Cluster0?retryWrites=true&w=majority',)
 
 app.use("/api", apiroutes)
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server Started...");
 })
