d6754e10a006f3afdbb95db7822056c7fc985e56
janvi18 <sjanvi9333@gmail.com>
2021-10-26 15:24:00 +0530

    .

--- model/signupModel.js
+++ model/signupModel.js
@@ -1,20 +1,21 @@
 var mongoose = require("mongoose")
 
 var signupSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
+    firstName: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    roleId:{
-        type:mongoose.Types.ObjectId
+    roleId: {
+        type: Number,
+        required: true
     }
 })
 
