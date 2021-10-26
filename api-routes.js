7ff9087d2e57fe56eaa2326e86c0e79169c6fd26
janvi18 <sjanvi9333@gmail.com>
2021-10-26 16:55:43 +0530

    .

--- api-routes.js
+++ api-routes.js
@@ -4,14 +4,12 @@ var signUpController = require("./controller/signupController")
 var examController = require("./controller/examController")
 
 var signUpController=require("./controller/signUpController")
-const examController=require("./controller/examController") 
+
 router.route("/signup").post(signUpController.saveuser)
 router.route("/login").post(signUpController.login)
 router.route("/users/:userId").get(signUpController.getuserByid)
 router.route("/updateUser").post(signUpController.updateUser)
 router.route("/forgotpassword").post(signUpController.forgotPassword)
-
-
 
 
 module.exports = router
